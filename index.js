d333d91945d1343a36726b1fcd363bcfa3f57091
Rik Pick~ <g29043066@gmail.com>
2023-11-12 15:04:09 +0200

    update

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on('message', async (msg) => {
   }
 });
 
-app.post('/web-data', async (req, res) => {
+app.post('/web-data/', async (req, res) => {
   const {queryId, products, totalPrice} = req.body;
 
   try {
