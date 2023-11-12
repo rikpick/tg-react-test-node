7e9e960df90c640f8c6bf7906e8d9ee0107b53e6
Rik Pick~ <g29043066@gmail.com>
2023-11-12 14:53:13 +0200

    data

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ bot.on('message', async (msg) => {
     try {
       const data = JSON.parse(msg?.web_app_data?.data)
       await bot.sendMessage(chatId, 'Спасибо за заказ!')
-      await bot.sendMessage(chatId, data.products)
-      await bot.sendMessage(409383842, data.products)
+      await bot.sendMessage(chatId, data)
+      await bot.sendMessage(chatId, data.totalPrice)
+
+      /*await bot.sendMessage(409383842, data.products)*/
 
     } catch (e) {
       console.log(e);
