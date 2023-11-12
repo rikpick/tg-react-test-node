9f8e5c7d4b23fa1bd224e97978d5b5c90fa93b47
Rik Pick~ <g29043066@gmail.com>
2023-11-12 16:38:04 +0200

    update1

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/web-data/', async (req, res) => {
      if(req.body) {
       const username = await bot.getWebAppData(queryId)
       const product = JSON.stringify(products)
-      bot.sendMessage(managerChatId, totalPrice.toString() + " " + product + " " + username.user);
+      bot.sendMessage(managerChatId, `${totalPrice} ${product} ${username.user}`);
      }
 
      return res.status(200).json({});
