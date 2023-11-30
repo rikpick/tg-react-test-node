cbad71b1ebe8609b3ab8aec4e2daa4f8b0a02a9f
Rik Pick~ <g29043066@gmail.com>
2023-11-30 20:14:59 +0200

    a

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/web-data/', async (req, res) => {
      if (req.body) {
       
         bot.sendMessage(managerChatId, 
-        `<strong>Заказ от:</strong> <a href="https://t.me/user?start=${username}">Покупатель</a>
+        `<strong>Заказ от:</strong> <a href="https://t.me/${name || 'user'}?start=${username}">Покупатель</a>
 <strong>На сумму:</strong> ${totalPrice} грн 
 <strong>Количество:</strong> ${products.map(item => item.title).join(', ')}
 <strong>Доставка на:</strong> ${address}
